e54effb8d981442a2352a11a58ce633bdf332d8e
kylo <kylo94@163.com>
2023-01-16 19:12:32 +0800

    create jingting

--- Test/JingTing.js
+++ Test/JingTing.js
@@ -1,2 +1,16 @@
-console.log('hello world 1');
-console.log('hello world 12');
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
