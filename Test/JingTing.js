3e1290adbc6a4d477f2a4fed041058a1656301ae
kylo <kylo94@163.com>
2023-01-16 19:25:08 +0800

    add license

--- Test/JingTing.js
+++ Test/JingTing.js
@@ -5,6 +5,7 @@
 // @description  优化使用体验
 // @author       You
 // @match        http://www.audio699.com/
+// @license      MIT
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=audio699.com
 // @grant        none
 // ==/UserScript==
